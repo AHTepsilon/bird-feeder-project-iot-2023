a8015212f744bff91155e90dd9edbc92ed4f90f7
AHTepsilon <alehertoro725@gmail.com>
2023-11-28 15:10:39 -0500

    website now receives the image from firebase and displays it

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {app, db, storage} from "./functions/firebase.js"
+import {app, db, storage, auth} from "./functions/firebase.js"
 import './App.css'
+import { signInAnonymously } from 'firebase/auth'
+import { ref, getDownloadURL, getMetadata } from 'firebase/storage'
 
 function GetPicture(){
   // Reference to Picture Storage Path
-const storageRef = ref(storage);
-var imgRef = storageRef.child('data/photo.jpg');
-
-firebase.auth().signInAnonymously().then(function() {
-
-  imgRef.getDownloadURL().then(function(url){
-    // `url` is the download URL for 'data/photo.jpg'
-    document.querySelector('img').src = url;
-    
-  }).catch(function(error) {
-    console.error(error);
-  });
-});
-
-imgRef.getMetadata()
-  .then((metadata) => {
-    console.log(metadata);
-    date = new Date(metadata.timeCreated);
-    console.log(date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate());
-    console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-    var time = (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-    var writtenDate = (date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate());
-    document.getElementById("date-time").innerHTML = time + " at " + writtenDate;
-  })
-  .catch((error)=> {
-    console.error(error);
-  });
 }
 
 function App() {
+  const storageRef = ref(storage);
+  const imgRef = ref(storage, 'data/photo.jpg');
+  
+  signInAnonymously(auth)
+  .then(() => {
+    getDownloadURL(imgRef)
+      .then((url) => {
+        document.getElementById('img').src = url;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+getMetadata(imgRef)
+  .then((metadata) => {
+    console.log(metadata);
+    const date = new Date(metadata.timeCreated);
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const writtenDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    document.getElementById('date-time').innerHTML = `${time} at ${writtenDate}`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
   return (
     <>
       <h1>Bird feeder</h1>
